f60b607bc189551b890fe31819d511645d71197d
Tom Bell <tjbell@gmail.com>
2020-06-30 12:22:05 -0600

    fix: improve printing json

--- utils.js
+++ utils.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 
 const message = (err, data) => {
   if (err) { console.log(`Error: ${err.message}`); }
-  else if (data) { console.log(`Success: ${JSON.stringify(data)}`); }
+  else if (data) { console.log(`Success: ${JSON.stringify(data, null, 2)}`); }
 };
 
 const readJSON = (filename) => fs.existsSync(`config/${filename}.json`) ? JSON.parse(fs.readFileSync(`config/${filename}.json`)) : undefined;
 
 module.exports = { message, readJSON };
-
